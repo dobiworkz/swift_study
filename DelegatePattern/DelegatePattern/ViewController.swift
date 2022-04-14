8af685874605c0f8c8bc3e3751595258c46be5e2
dobi <dobi@kakao.com>
2022-04-14 20:18:53 +0900

    델리게이트패턴 연습

--- DelegatePattern/DelegatePattern/ViewController.swift
+++ DelegatePattern/DelegatePattern/ViewController.swift
@@ -7,14 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+//UITextFieldDelegate protocol inherit
     
     @IBOutlet weak var textField: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textField.delegate = self
+        //textField의 대행자(delegate)를 뷰컨트롤러로 설정
         
         setUp()
     }
@@ -29,6 +32,30 @@ class ViewController: UIViewController {
     }
 
     @IBAction func doneButtonTapped(_ sender: UIButton) {
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print(#function)
+    }
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print(string)
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print(#function)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField.text == "" {
+            textField.placeholder = "Type Sometheing"
+            return false
+        } else {
+            return true
+        }
     }
     
 }
