f964e8ba3c2822282009a2765be022fca5a3299c
dobi <dobi@kakao.com>
2022-04-18 18:12:44 +0900

    BMI 로직 추가

--- BMI/BMI/ResultViewController.swift
+++ BMI/BMI/ResultViewController.swift
@@ -16,11 +16,14 @@ class ResultViewController: UIViewController {
     
     @IBOutlet weak var backButton: UIButton!
     
+    var height: String?
+    var weight: String?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        makeUI()
     }
     
     
@@ -33,6 +36,38 @@ class ResultViewController: UIViewController {
         
         bmiNumberLabel.clipsToBounds = true
         bmiNumberLabel.layer.cornerRadius = 8
+        
+        calculate()
+    }
+    
+    func calculate(){
+        guard let w = Double(weight!), let h = Double(height!) else{ return }
+        
+        var bmi = w / ( h * h ) * 10000
+        bmi = round(bmi * 10) / 10
+        
+        bmiNumberLabel.text = String(bmi)
+        switch bmi{
+        case ..<18.5 :
+            adviceLabel.text = "저체중"
+            bmiNumberLabel.backgroundColor = #colorLiteral(red: 0, green: 0.9914394021, blue: 1, alpha: 1)
+        case 18.6..<22.9 :
+            adviceLabel.text = "표준체중"
+            bmiNumberLabel.backgroundColor = #colorLiteral(red: 0.8321695924, green: 0.985483706, blue: 0.4733308554, alpha: 1)
+        case 23.0..<24.9 :
+            adviceLabel.text = "과체중"
+            bmiNumberLabel.backgroundColor = #colorLiteral(red: 1, green: 0.5409764051, blue: 0.8473142982, alpha: 1)
+        case 25.0..<29.9 :
+            adviceLabel.text = "중도비만"
+            bmiNumberLabel.backgroundColor = #colorLiteral(red: 1, green: 0.1857388616, blue: 0.5733950138, alpha: 1)
+        case 30.0... :
+            adviceLabel.text = "고도비만"
+            bmiNumberLabel.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+        default :
+            break
+        }
+        
+        
     }
     
     
--- BMI/BMI/ViewController.swift
+++ BMI/BMI/ViewController.swift
@@ -41,9 +41,66 @@ class ViewController: UIViewController  {
     
     @IBAction func calculateButtonTapped(_ sender: UIButton) {
     }
+
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        
+        if heightTextField.text == "" || weightTextField.text == "" {
+            mainLabel.text="키와 몸무게를 반드시 입력해야 합니다."
+            mainLabel.textColor = .red
+            return false
+        }
+        
+        mainLabel.text="키와 몸무게를 입력해주세요"
+        mainLabel.textColor = .black
+        
+        return true
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toResultVC" {
+            let resultView = segue.destination as! ResultViewController
+            
+            resultView.height = heightTextField.text
+            resultView.weight = weightTextField.text
+        }
+        
+        heightTextField.text = ""
+        weightTextField.text = ""
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        weightTextField.resignFirstResponder()
+        heightTextField.resignFirstResponder()
+    }
     
 }
 
 extension ViewController: UITextFieldDelegate{
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        // 숫자만 입력되도록, ""를 허용하는 이유는 글자 삭제시 true를 리턴하기 위함
+        if Int(string) != nil || string == "" {
+            return true
+        }
+        
+        return false
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if heightTextField.text != "", weightTextField.text != "" {
+            weightTextField.resignFirstResponder()
+            return true
+        } else if heightTextField.text != "" {
+            weightTextField.becomeFirstResponder()
+            return true
+        }
+        
+        return false
+    }
+    
     
 }
